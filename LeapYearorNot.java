f6ae07a87ec55fc96266cf8958586a47ce6eb9cc
Prakash <93327731+lkprakash@users.noreply.github.com>
2022-04-26 20:26:07 +0530

    Create LeapYearorNot.java

---
+++ LeapYearorNot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+   public static void main(String[] args){
+      int year;
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+
+      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)){
+          System.out.println("Specified year is a leap year");
+      }
+         
+      else{
+          System.out.println("Specified year is not a leap year");
+      }
+         
+   }
+}
